da4db53084a861a73554607292a77f42876559f6
D Sofer <d@sofer.com>
2013-07-31 00:20:59 +0100

    ed

--- js/tracking.js
+++ js/tracking.js
@@ -2,12 +2,12 @@ var filetypes = ["pdf", "ppt", "doc", "docx", "pptx", "xls", "xlsx"];
 
 $jppc(document).ready(function() {
   $jppc("a").attr("onClick", function() {
-    alert(this.href);
     if (this.href.substr(0,7) != "http://" && this.href.substr(0,8) != "https://" ) { 
       var url = this.href.substr(this.href.lastIndexOf("/") + 1);
       //var url = this.href; // uncommend this to record full paths
       var ext = url.substr(url.lastIndexOf(".") + 1).toLowerCase();
       if ($jppc.inArray(ext, filetypes) > -1) {
+        alert(url);
         return "_gaq.push(['_trackPageview', '" + url + "']);"
       }
     }
