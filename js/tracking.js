fcfbf018ffbe2bccccb38a440beda6d08da29b01
D Sofer <d@sofer.com>
2013-07-31 07:44:03 +0100

    ed

--- js/tracking.js
+++ js/tracking.js
@@ -2,7 +2,6 @@ var filetypes = ["pdf", "ppt", "doc", "docx", "pptx", "xls", "xlsx"];
 
 $jppc(document).ready(function() {
   $jppc("a").attr("onClick", function() {
-    alert(this.href);
     if (this.href.substr(0,7) != "http://" && this.href.substr(0,8) != "https://" ) { 
       var url = this.href.substr(this.href.lastIndexOf("/") + 1);
       //var url = this.href; // uncommend this to record full paths
