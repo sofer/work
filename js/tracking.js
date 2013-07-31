556163b856713f260db43d8ff79482baff13e57b
D Sofer <d@sofer.com>
2013-07-31 07:56:27 +0100

    ed

--- js/tracking.js
+++ js/tracking.js
@@ -1,8 +1,9 @@
 var filetypes = ["pdf", "ppt", "doc", "docx", "pptx", "xls", "xlsx"];
+var host = "blog.sofer.com";
 
 $jppc(document).ready(function() {
   $jppc("a").attr("onClick", function() {
-    if (this.href.substr(0,7) != "http://" && this.href.substr(0,8) != "https://" ) { 
+    if (this.host == host) { // ignore external links
       var url = this.href.substr(this.href.lastIndexOf("/") + 1);
       //var url = this.href; // uncommend this to record full paths
       var ext = url.substr(url.lastIndexOf(".") + 1).toLowerCase();
