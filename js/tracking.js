e24beca70b9801c6d09b493a84c108123da8f303
D Sofer <d@sofer.com>
2013-07-31 09:00:13 +0100

    ed

--- js/tracking.js
+++ js/tracking.js
@@ -1,16 +1,15 @@
-var host = "blog.sofer.com";
 var filetypes = ["pdf", "ppt", "doc", "docx", "pptx", "xls", "xlsx"];
+var host = "blog.sofer.com";                    // remove
+//var host = "http://www.womenandchildrenfirst.org.uk";
 
 $jppc(document).ready(function() {
   $jppc("a").attr("onClick", function() {
-    if (this.host == host) { // ignore external links
-      pathStartIndex = host.length + 7;
-      var url = this.href.substr(pathStartIndex);
-      //var url = this.href.substr(this.href.lastIndexOf("/") + 1);
-      //var url = this.href; // uncommend this to record full paths
-      var ext = url.substr(url.lastIndexOf(".") + 1).toLowerCase();
-      if ($jppc.inArray(ext, filetypes) > -1) {
-        return "_gaq.push(['_trackPageview', '" + url + "']);"
+    if (this.host == host) {                    // ignore external links
+      pathStartIndex = host.length + 7;         //ignore first part of the href
+      var path = this.href.substr(pathStartIndex);
+      var ext = path.substr(path.lastIndexOf(".") + 1).toLowerCase();
+      if ($jppc.inArray(ext, filetypes) > -1) { // -1 means item not in array
+        return "_gaq.push(['_trackPageview', '" + path + "']);"
       }
     }
   })
