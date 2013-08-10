68ac6f93730466020611233b6397a2584220a527
D Sofer <d@sofer.com>
2013-08-10 22:45:23 +0100

    ed

--- js/tracking.js
+++ js/tracking.js
@@ -1,14 +1,13 @@
 var filetypes = ["pdf", "ppt", "doc", "docx", "pptx", "xls", "xlsx"];
-var host = 'http://www.womenandchildrenfirst.org.uk';
+var domain = 'http://www.womenandchildrenfirst.org.uk';
 
 $jppc(document).ready(function($) {
 
   $("a").attr("onClick", function() {
-    if (this.host == host) {                // ignore external links
+    if (this.domain == domain) {            // ignore external links
       pathStartIndex = host.length + 7;     // ignore first part of the href
       var path = this.href.substr(pathStartIndex);
       var ext = path.substr(path.lastIndexOf(".") + 1).toLowerCase();
-      alert('this should work: '+path);
       if ($.inArray(ext, filetypes) > -1) { // -1 means item not in array
         return "_gaq.push(['_trackPageview', '" + path + "']);"
       }
