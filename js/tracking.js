cf2dc8288f25ed12c50c5e3fc6c2d5a226bcdc0b
D Sofer <d@sofer.com>
2013-08-10 22:24:22 +0100

    ed

--- js/tracking.js
+++ js/tracking.js
@@ -1,10 +1,11 @@
 var filetypes = ["pdf", "ppt", "doc", "docx", "pptx", "xls", "xlsx"];
+var host = 'http://www.womenandchildrenfirst.org.uk';
 
 $jppc(document).ready(function($) {
 
   $("a").attr("onClick", function() {
-    if (this.host == Document.domain) {         // ignore external links
-      pathStartIndex = this.host.length + 7;    //ignore first part of the href
+    if (this.host == host) {                // ignore external links
+      pathStartIndex = host.length + 7;     // ignore first part of the href
       var path = this.href.substr(pathStartIndex);
       var ext = path.substr(path.lastIndexOf(".") + 1).toLowerCase();
       if ($.inArray(ext, filetypes) > -1) { // -1 means item not in array
