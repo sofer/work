3967b424cb1883a518a0c323c8579149455314b0
D Sofer <d@sofer.com>
2013-08-10 23:03:25 +0100

    ed

--- js/tracking.js
+++ js/tracking.js
@@ -4,8 +4,8 @@ var domain = 'www.womenandchildrenfirst.org.uk';
 $jppc(document).ready(function($) {
 
   $("a").attr("onClick", function() {
-    if (this.domain == domain) {            // ignore external links
-      pathStartIndex = domain.length;       // ignore first part of the href
+    if (this.host == domain) {              // ignore external links
+      pathStartIndex = domain.length + 7;   // ignore first part of the href
       var path = this.href.substr(pathStartIndex);
       var ext = path.substr(path.lastIndexOf(".") + 1).toLowerCase();
       if ($.inArray(ext, filetypes) > -1) { // -1 means item not in array
